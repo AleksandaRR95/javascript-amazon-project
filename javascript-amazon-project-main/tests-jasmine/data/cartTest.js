367f1efeda2d7924abf0bddb2e661e99a9ab8370
AleksandaRR95 <aleksandar.moldovan123@gmail.com>
2025-06-05 10:35:32 +0200

    Add tests for removing products from the cart in cartTest.js

--- javascript-amazon-project-main/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project-main/tests-jasmine/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage, addProductToCart } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, addProductToCart, removeFromCart } from "../../data/cart.js";
 import { deliveryOptions } from "../../data/deliveryOptions.js";
 
 describe('test suite: addProductToCart', () => {
@@ -32,5 +32,40 @@ describe('test suite: addProductToCart', () => {
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
   });
+
+  
 });
 
+describe('test suite: removes product from cart', () => {
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+  const productId3= '83d4ca15-0f35-48f5-b7a3-1ea210004f2e';
+  beforeEach(() => {
+       spyOn(localStorage, 'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: '1'
+        },{
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: '2'
+        }
+      ])
+    });
+  });
+
+    it('removes product that is in cart', () => {
+    removeFromCart(productId1);
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId2);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  })
+  it('removes product that is not in cart', () => {
+    removeFromCart(productId3);
+    expect(cart.length).toEqual(2);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+});
